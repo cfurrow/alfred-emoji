6b38c7d3a866c6b505fc9dcd3ce7a73612c45ce9
Carl Furrow <hello@carlfurrow.com>
2022-01-09 11:58:18 -0500

    Fix sed in build script
    I don't understand how this was working. Maybe the previous versions of macOS had an odd version of sed?

--- build.sh
+++ build.sh
@@ -24,16 +24,16 @@ cp icons/beer.png ./icon.png
 
 echo "Updating version ..."
 curVersion=$(node -e "console.log(require('${parentDir}/package.json').version)")
-sed -i '' 's/{{version}}/'${curVersion}'/' info.plist
+sed -i 's/{{version}}/'${curVersion}'/' info.plist
 
 echo "Injecting readme ..."
 readme="${parentDir}/src/Readme.md"
-sed -i '' -e "/{{readme}}/{r ${readme}" -e 'd' -e '}' info.plist
+sed -i -e "/{{readme}}/{r ${readme}" -e 'd' -e '}' info.plist
 
 echo "Injecting auto-update script ..."
 update="$(mktemp)"
 cat ${parentDir}/src/update.sh | sed 's/&/\&amp;/g; s/</\&lt;/g; s/>/\&gt;/g' > ${update}
-sed -i '' -e "/{{update_script}}/{r ${update}" -e 'd' -e '}' info.plist
+sed -i -e "/{{update_script}}/{r ${update}" -e 'd' -e '}' info.plist
 
 echo "Bundling workflow ..."
 zip -Z deflate -rq9 ${parentDir}/alfred-emoji.alfredworkflow * -x etc
